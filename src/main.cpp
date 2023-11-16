2da34b641188013b353462f1ae03280c22149a1e
André Favotto <andrefavotto@hotmail.com>
2023-11-16 16:58:36 -0300

    Modified from RTU to TCP-IP and changed all registers to HREG

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 #include <Arduino.h>
-#include <ModbusRTU.h>
-
-/* Global constants */
+#include <WiFi.h>
+#include <ModbusIP_ESP8266.h>
 
 #define TANK_LEVEL_MIN_THRESHOLD 100
 #define TANK_LEVEL_MAX_THRESHOLD 3800
@@ -9,92 +8,122 @@
 /* Modbus pre-processing settings */
 
 #define SLAVE_ID 1
+
 //Coil, Discrete Input, Input Register, Holding Register
-#define COIL_BASE_ADDRESS       1
-#define BUILTIN_LED_COIL        (1 - COIL_BASE_ADDRESS)         //Digital output - built-in LED
+// #define COIL_BASE_ADDRESS       1
+// #define ALARM_COIL        (1 - COIL_BASE_ADDRESS)         //Digital output - built-in LED
 
-#define I_STATUS_BASE_ADDRESS   10001
-#define LEVEL_SWITCH_I_COIL     (10001 - I_STATUS_BASE_ADDRESS) //Digital input
+// #define I_STATUS_BASE_ADDRESS   10001
+// #define LEVEL_SWITCH_I_COIL     (10001 - I_STATUS_BASE_ADDRESS) //Digital input
 
-#define I_REG_BASE_ADDRESS      30001
-#define TANK_LEVEL_I_REG        (30001 - I_REG_BASE_ADDRESS)    //Analog input (read only)
-#define DIAGNOSTICS_I_REG       (30002 - I_REG_BASE_ADDRESS)    //Analog input (read only)
+// #define I_REG_BASE_ADDRESS      30001
+// #define TANK_LEVEL_I_REG        (30001 - I_REG_BASE_ADDRESS)    //Analog input (read only)
+// #define DIAGNOSTICS_I_REG       (30002 - I_REG_BASE_ADDRESS)    //Analog input (read only)
 
 #define HREG_BASE_ADDRESS       40001
-#define VALVE_SIGNAL_HREG       (40001 - HREG_BASE_ADDRESS)     //Analog output
+#define ALARM_COIL              (40001 - HREG_BASE_ADDRESS)      //Digital output
+#define LEVEL_SWITCH_I_COIL     (40002 - HREG_BASE_ADDRESS)      //Digital input
+#define TANK_LEVEL_I_REG        (40003 - HREG_BASE_ADDRESS)      //Analog input (read only)
+#define VALVE_SIGNAL_HREG       (40004 - HREG_BASE_ADDRESS)      //Analog output
+#define DIAGNOSTICS_I_REG       (40005 - HREG_BASE_ADDRESS)      //Analog input (read only)
 
 /* Pinout */
-#define LEVEL_SWITCH_PIN  15
-#define TANK_LEVEL_PIN 4
-#define LED_BUILTIN 2
+#define LEVEL_SWITCH_PIN  34
+#define TANK_LEVEL_PIN 35
+#define ALARM_BUZZER 32
+#define VALVE_SIGNAL_LEDBUILTIN 2
 
 /* Global variables*/
   
-ModbusRTU mb;
-bool led_status;   /* false for off and true for on */
-bool level_switch; /* false for closed and true for open */
+ModbusIP mb;
+bool alarm_status;      /* false for off and true for on */
+bool level_switch;      /* false for closed and true for open */
 uint16_t tank_level;
-uint16_t led_brightness; /* 0 to 255 */
+uint16_t valve_output;  /* 0 to 255 */
 uint16_t diagnostics;   /* 1 if potentiometer is disconnected */
 size_t timestamp;
 
 /* Callback functions */
 
-static uint16_t cb_led_builtin_coil(TRegister* reg, uint16_t val)
+static uint16_t cb_alarm_buzzer_coil(TRegister* reg, uint16_t val)
 {
-  led_status = (val != 0);
-  digitalWrite(LED_BUILTIN, led_status);
+  alarm_status = (val != 0);
+  digitalWrite(ALARM_BUZZER, alarm_status);
+  Serial.println("Alarm status changed");
   return val;
 }
 
 static uint16_t cb_valve_signal(TRegister* reg, uint16_t val)
 {
   //scale val from 0-65535 to 0-255
-  led_brightness = map(val, 0, 65535, 0, 255);
-  analogWrite(LED_BUILTIN, led_brightness);
+  valve_output = map(val, 0, 65535, 0, 255);
+  analogWrite(VALVE_SIGNAL_LEDBUILTIN, valve_output);
   return val;
 }
 
 void setup() {
+  /* Connect to network*/
+  Serial.begin(9600);
+  WiFi.begin("LinkBox", "industria50");
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("Connecting to WiFi..");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
    /* Set initial values */
 
   tank_level = 0;
-  led_brightness = 0;
-  led_status = false;
+  valve_output = 0;
+  alarm_status = false;
   level_switch = false;
   diagnostics = 0;
 
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(LEVEL_SWITCH_PIN, INPUT);
+  pinMode(ALARM_BUZZER, OUTPUT);
+  pinMode(VALVE_SIGNAL_LEDBUILTIN, OUTPUT);
+  pinMode(LEVEL_SWITCH_PIN, INPUT_PULLDOWN);
   pinMode(TANK_LEVEL_PIN, INPUT);
 
-  Serial.begin(9600, SERIAL_8N1);
-  mb.begin(&Serial);
-  //mb.begin(&Serial, RXTX_PIN);  //or use RX/TX direction control pin (if required)
-  mb.slave(SLAVE_ID);
+  // /* Add coil status */
+  // mb.addCoil(ALARM_COIL);
+  // mb.Coil(ALARM_COIL, alarm_status);
+  // mb.onSetCoil(ALARM_COIL, cb_alarm_buzzer_coil, 1);
 
-  /* Add coil status */
-  mb.addCoil(BUILTIN_LED_COIL);
-  mb.Coil(BUILTIN_LED_COIL, led_status);
-  mb.onSetCoil(BUILTIN_LED_COIL, cb_led_builtin_coil, 1);
+  // /* Add input status */
+  // mb.addIsts(LEVEL_SWITCH_I_COIL);
+  // mb.Ists(LEVEL_SWITCH_I_COIL, level_switch);
 
-  /* Add input status */
-  mb.addIsts(LEVEL_SWITCH_I_COIL);
-  mb.Ists(LEVEL_SWITCH_I_COIL, level_switch);
+  // /* Add input registers */
+  // mb.addIreg(TANK_LEVEL_I_REG);
+  // mb.Ireg(TANK_LEVEL_I_REG, tank_level);
 
-  /* Add input registers */
-  mb.addIreg(TANK_LEVEL_I_REG);
-  mb.Ireg(TANK_LEVEL_I_REG, tank_level);
-
-  mb.addIreg(DIAGNOSTICS_I_REG);
-  mb.Ireg(DIAGNOSTICS_I_REG, diagnostics);
+  // mb.addIreg(DIAGNOSTICS_I_REG);
+  // mb.Ireg(DIAGNOSTICS_I_REG, diagnostics);
 
   /* Add holding register */
 
+  mb.addHreg(ALARM_COIL);
+  mb.Hreg(ALARM_COIL, alarm_status);
+  mb.onSetHreg(ALARM_COIL  , cb_alarm_buzzer_coil, 1);
+
+  mb.addHreg(LEVEL_SWITCH_I_COIL);
+  mb.Hreg(LEVEL_SWITCH_I_COIL  , level_switch);
+
+  mb.addHreg(TANK_LEVEL_I_REG);
+  mb.Hreg(TANK_LEVEL_I_REG  , tank_level);
+
   mb.addHreg(VALVE_SIGNAL_HREG);
-  mb.Hreg(VALVE_SIGNAL_HREG  , led_brightness);
+  mb.Hreg(VALVE_SIGNAL_HREG  , valve_output);
   mb.onSetHreg(VALVE_SIGNAL_HREG  , cb_valve_signal, 1);
 
+  mb.addHreg(DIAGNOSTICS_I_REG);
+  mb.Hreg(DIAGNOSTICS_I_REG  , diagnostics);
+  
+  //initialize modbus connection
+  mb.server();
 }
 
 
@@ -105,10 +134,10 @@ void loop() {
       timestamp = millis();
 
       tank_level = analogRead(TANK_LEVEL_PIN);
-      mb.Ireg(TANK_LEVEL_I_REG, tank_level);
+      mb.Hreg(TANK_LEVEL_I_REG, tank_level);
 
       level_switch = digitalRead(LEVEL_SWITCH_PIN);
-      mb.Ists(LEVEL_SWITCH_I_COIL, level_switch);
+      mb.Hreg(LEVEL_SWITCH_I_COIL, level_switch);
 
       /* Diagnostics value update */
 
@@ -126,7 +155,7 @@ void loop() {
           diagnostics = 0;
         }
 
-      mb.Ireg(DIAGNOSTICS_I_REG, diagnostics);
+      mb.Hreg(DIAGNOSTICS_I_REG, diagnostics);
     }
 
   mb.task();
